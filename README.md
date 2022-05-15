f4ef8e641a7968e0caafbc625107d4c327bf0b8c
Kevin Wang <59260907+Togohogo1@users.noreply.github.com>
2022-05-15 10:06:12 -0400

    basic GA end condition

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # Travelling Salesman Problem
 Various methods of solving the Travelling Salesman Problem with visualization
 
+Intended use: genetic algorithm testing, plotting a valid solution (dp or bf) or a very good solution (sim anneal)
+
+Outline
+- how to use plotter and solution
+
 ### Todo
 - Finish the readme with proper images
 - Write up c version of dynamic programming solution
--- src/genetic_algorithm.py
+++ src/genetic_algorithm.py
@@ -1,10 +1,14 @@
 from population import Population
 
 def run(x_coord, y_coord, dist_from, city_count, solution=None):
-    population = Population(1000, 0.01)
+    population_size = 1000
+    mutation_rate = 0.01
+    max_generations = 2000
+
+    population = Population(population_size, mutation_rate)
     population.initialize(dist_from, city_count)
 
-    while True:
+    while population.generation < max_generations:
         population.select()
         population.evaluate(dist_from, x_coord, y_coord, solution)
         population.crossover(dist_from, city_count)  # And mutate
